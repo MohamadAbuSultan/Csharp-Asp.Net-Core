6eff794a858b1f5d0ff28f96c15cd321ade02316
MohamadKhaled <mohamadknsult2@gmail.com>
2023-09-29 18:54:17 +0300

    September-2023

--- csharp_fundamentals/CSharpFundamentals.ConsoleApp/Methods.cs
+++ csharp_fundamentals/CSharpFundamentals.ConsoleApp/Methods.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            CalculateAverage(numbers);
+            numbers = new int[] { 10, 20, 30, 40, 50 };
+            CalculateAverage(numbers);
+        }
+        static void CalculateAverage(int[] numbers)
+        {
+            int number = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                number += numbers[i];
+            }
+            double avg = number / numbers.Length;
+            Console.WriteLine(avg);
         }
     }
 }
