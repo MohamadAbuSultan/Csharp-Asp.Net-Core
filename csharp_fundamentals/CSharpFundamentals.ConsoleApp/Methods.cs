33f1bb2534f97d7c56de105a82e3dfd6b87d385b
MohamadKhaled <mohamadknsult2@gmail.com>
2023-09-30 08:45:32 +0300

    September-2023

--- csharp_fundamentals/CSharpFundamentals.ConsoleApp/Methods.cs
+++ csharp_fundamentals/CSharpFundamentals.ConsoleApp/Methods.cs
@@ -4,10 +4,14 @@
     {
         static void Main(string[] args)
         {
+            //int[] numbers = { 1, 2, 3, 4, 5 };
+            //CalculateAverage(numbers);
+            //numbers = new int[] { 10, 20, 30, 40, 50 };
+            //CalculateAverage(numbers);
             int[] numbers = { 1, 2, 3, 4, 5 };
-            CalculateAverage(numbers);
+            CalculateAverage1(numbers, printAverageToConsole: true);
             numbers = new int[] { 10, 20, 30, 40, 50 };
-            CalculateAverage(numbers);
+            CalculateAverage1(numbers, printSumToConsole: true);
         }
         static void CalculateAverage(int[] numbers)
         {
@@ -18,6 +22,20 @@
             }
             double avg = number / numbers.Length;
             Console.WriteLine(avg);
+            return; // عادي لو كتبتها في الدالة إلي ما بترجع قيمة ، بنكتبها عشان يتجاهل السطور إلي بعدها
+        }
+        // optional arguments لازم يكون آخر إشي تضيفه
+        static double CalculateAverage1(int[] numbers, bool printAverageToConsole = false /*this is optional arguments*/, bool printSumToConsole = false)
+        {
+            int number = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                number += numbers[i];
+            }
+            double average = number / numbers.Length;
+            if (printSumToConsole == true) Console.WriteLine("Sum is : " + number);
+            if (printAverageToConsole == true) Console.WriteLine($"average is : {average}");
+            return average;
         }
     }
 }
