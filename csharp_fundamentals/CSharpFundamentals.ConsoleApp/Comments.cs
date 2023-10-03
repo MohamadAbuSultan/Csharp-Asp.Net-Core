cecaaeb0fab724109b3465331a7351b5910f071a
MohamadKhaled <mohamadknsult2@gmail.com>
2023-10-03 23:48:10 +0300

    .

---
+++ csharp_fundamentals/CSharpFundamentals.ConsoleApp/Comments.cs
@@ -0,0 +1,25 @@
+﻿namespace CSharpFundamentals
+{
+    internal class Comments
+    {
+
+        // إلي بملفك todo ببينلك كل ال ctrl \ t لما تكتب تو دو بتضغط
+        // Task list <= view أو بتروح على 
+        // todo: hello 
+
+        // methods إلي تحت هالسطر بملف document كتبت عن ال
+        /// to document any method
+        /// <summary>
+        /// Calculate the average for an array of numbers 
+        /// </summary>
+        /// <param name="numbers">the array to calculate its average</param>
+        /// <param name="printAverageToConsole">determine whether print average nor not</param>
+        /// <param name="printSumToConsole">determine whether print sum nor not</param>
+        /// <returns></returns>
+        /// 
+
+
+        // todo: hello
+
+    }
+}
--- csharp_fundamentals/CSharpFundamentals.ConsoleApp/Methods.cs
+++ csharp_fundamentals/CSharpFundamentals.ConsoleApp/Methods.cs
@@ -2,7 +2,7 @@
 {
     internal class Methods
     {
-        static void Main(string[] args)
+        static void Main9(string[] args)
         {
             //int[] numbers = { 1, 2, 3, 4, 5 };
             //CalculateAverage(numbers);
@@ -12,6 +12,8 @@
             CalculateAverage1(numbers, printAverageToConsole: true);
             numbers = new int[] { 10, 20, 30, 40, 50 };
             CalculateAverage1(numbers, printSumToConsole: true);
+
+            CalculateAverage1(numbers);
         }
         static void CalculateAverage(int[] numbers)
         {
@@ -24,7 +26,18 @@
             Console.WriteLine(avg);
             return; // عادي لو كتبتها في الدالة إلي ما بترجع قيمة ، بنكتبها عشان يتجاهل السطور إلي بعدها
         }
+
+
+
         // optional arguments لازم يكون آخر إشي تضيفه
+        /// to document any method
+        /// <summary>
+        /// Calculate the average for an array of numbers 
+        /// </summary>
+        /// <param name="numbers">the array to calculate its average</param>
+        /// <param name="printAverageToConsole">determine whether print average nor not</param>
+        /// <param name="printSumToConsole">determine whether print sum nor not</param>
+        /// <returns></returns> 
         static double CalculateAverage1(int[] numbers, bool printAverageToConsole = false /*this is optional arguments*/, bool printSumToConsole = false)
         {
             int number = 0;
