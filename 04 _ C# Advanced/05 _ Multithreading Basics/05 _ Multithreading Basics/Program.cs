52fe645214f24d9718392ac4a0d735de6783af48
MohamadKhaled <mohamadknsult2@gmail.com>
2024-07-04 19:06:01 +0300

    Multithreading Basics

---
+++ 04 _ C# Advanced/05 _ Multithreading Basics/05 _ Multithreading Basics/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Multithreading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var th1 = new Thread(ProcessBatch1);
+            //th1.Priority = ThreadPriority.Highest;
+            //var th2 = new Thread(ProcessBatch2);
+            //th2.Priority = ThreadPriority.Lowest;
+            //th1.Start();
+            //th1.IsBackground = true;
+            //th2.IsBackground = true;
+            //th2.Start();
+
+            var cts = new CancellationTokenSource();
+            ThreadPool.QueueUserWorkItem(ProcessBatch1, cts.Token);
+            ThreadPool.QueueUserWorkItem(ProcessBatch2, cts.Token);
+            Thread.Sleep(1200);
+            cts.Cancel();
+        }
+
+        private static object _lock = new();
+
+        private static void ProcessBatch1(object? state)
+        {
+            Thread.Sleep(1000);
+            for (var i = 1; i <= 1000; i++)
+            {
+                lock (_lock)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write($" {i} ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+        }
+        private static void ProcessBatch2(object? state)
+        {
+            Thread.Sleep(1000);
+            for (var i = 1001; i <= 2000; i++)
+            {
+                lock (_lock)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write($" {i} ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+        }
+    }
+}
