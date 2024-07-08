370ddf9ef1afd5ab6096b99b8e77049528584c81
MohamadKhaled <mohamadknsult2@gmail.com>
2024-07-08 21:38:26 +0300

    July-2024

--- 04 _ C# Advanced/06 _ Task-Based Asynchronous Pattern (TAP)/06 _ Task-Based Asynchronous Pattern (TAP)/Program.cs
+++ 04 _ C# Advanced/06 _ Task-Based Asynchronous Pattern (TAP)/06 _ Task-Based Asynchronous Pattern (TAP)/Program.cs
@@ -2,25 +2,43 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var cts = new CancellationTokenSource();
             Console.WriteLine("Main Thread Id: " + Environment.CurrentManagedThreadId);
-            ProcessBatch1(cts.Token);
-            ProcessBatch2(cts.Token);
+            //await ProcessBatch1(cts.Token);
+            var task1 = ProcessBatch1(cts.Token);
+            //var task2 = ProcessBatch2(cts.Token);
+            //await task1;
+            //await task2;
+
+            var task2 = await task1.ContinueWith(async t => await ProcessBatch2(cts.Token));
+
+            //var x = ProcessBatch1(cts.Token);
+            //var y = await ProcessBatch1(cts.Token);
+
+            await Task.WhenAll(task1, task2);
+            //await Task.WhenAny(task1, task2);
+
+
+            Console.WriteLine("Enter Your Name: ");
+            var Name = Console.ReadLine();
+            Console.WriteLine($"Welcome, {Name} !");
+            Console.ReadKey();
         }
 
         private static object _lock = new();
 
-        private static async Task ProcessBatch1(CancellationToken cancellationToken)
+        private static async Task<int> ProcessBatch1(CancellationToken cancellationToken)
         {
             Console.WriteLine("Batch1 Thread Id: " + Environment.CurrentManagedThreadId);
-            await Task.Delay(1);
             Console.WriteLine("Batch1 Thread Id: " + Environment.CurrentManagedThreadId);
             for (var i = 1; i <= 100; i++)
             {
                 if (cancellationToken.IsCancellationRequested)
-                    return;
+                    return 0;
+
+                await Task.Delay(100);
 
                 lock (_lock)
                 {
@@ -29,17 +47,18 @@
                     Console.ForegroundColor = ConsoleColor.White;
                 }
             }
-            return;
+            return 100;
         }
         private static async Task ProcessBatch2(CancellationToken cancellationToken)
         {
             Console.WriteLine("\nBatch2 Thread Id: " + Environment.CurrentManagedThreadId);
-            await Task.Delay(1);
             Console.WriteLine("\nBatch2 S2 Thread Id: " + Environment.CurrentManagedThreadId);
             for (var i = 101; i <= 200; i++)
             {
                 if (cancellationToken.IsCancellationRequested)
                     return;
+
+                await Task.Delay(100);
 
                 lock (_lock)
                 {
--- 04 _ C# Advanced/06 _ Task-Based Asynchronous Pattern (TAP)/06 _ Task-Based Asynchronous Pattern (TAP)/obj/Debug/net8.0/06 _ Task-Based Asynchronous Pattern (TAP).AssemblyInfo.cs
+++ 04 _ C# Advanced/06 _ Task-Based Asynchronous Pattern (TAP)/06 _ Task-Based Asynchronous Pattern (TAP)/obj/Debug/net8.0/06 _ Task-Based Asynchronous Pattern (TAP).AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("06 _ Task-Based Asynchronous Pattern (TAP)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35fdafdfe0ac8659baaf4b9243ce445390b4ee89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49d754f818527aba077bed1636064122cb3a154e")]
 [assembly: System.Reflection.AssemblyProductAttribute("06 _ Task-Based Asynchronous Pattern (TAP)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("06 _ Task-Based Asynchronous Pattern (TAP)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
