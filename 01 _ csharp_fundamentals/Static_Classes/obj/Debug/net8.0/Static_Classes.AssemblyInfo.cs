e8a97c2f493af074ed5a5743c84a848f77a3580a
MohamadKhaled <mohamadknsult2@gmail.com>
2025-02-03 21:36:08 +0200

    .

--- 01 _ csharp_fundamentals/Static_Classes/obj/Debug/net8.0/Static_Classes.AssemblyInfo.cs
+++ 01 _ csharp_fundamentals/Static_Classes/obj/Debug/net8.0/Static_Classes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Static_Classes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51cd424f77dbc35265dab9fa423bcfdd33524587")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8389ff402f1794b0ed0113e774016fd93c3f611")]
 [assembly: System.Reflection.AssemblyProductAttribute("Static_Classes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Static_Classes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
