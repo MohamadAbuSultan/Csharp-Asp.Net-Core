4c7ad1fde20d43fed6745914908a404de9db5937
MohamadKhaled <mohamadknsult2@gmail.com>
2025-02-28 06:43:40 +0200

    test

---
+++ ASP.NET Core/Web_API/f/Creating and Exploring NET Core Web API/Controller/ProductController.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductController : ControllerBase
+{
+    private static List<Product> products = new List<Product>();
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Product>> GetAll()
+    {
+        return Ok(products);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Product> GetById(int id)
+    {
+        var product = products.FirstOrDefault(p => p.Id == id);
+        if (product == null)
+            return NotFound();
+
+        return Ok(product);
+    }
+
+    [HttpPost]
+    public ActionResult<Product> Create(Product newProduct)
+    {
+        newProduct.Id = products.Count + 1;
+        products.Add(newProduct);
+        return CreatedAtAction(nameof(GetById), new { id = newProduct.Id }, newProduct);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Product updatedProduct)
+    {
+        var product = products.FirstOrDefault(p => p.Id == id);
+        if (product == null)
+            return NotFound();
+
+        product.Name = updatedProduct.Name;
+        product.Description = updatedProduct.Description;
+        product.Price = updatedProduct.Price;
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var product = products.FirstOrDefault(p => p.Id == id);
+        if (product == null)
+            return NotFound();
+
+        products.Remove(product);
+        return NoContent();
+    }
+}
