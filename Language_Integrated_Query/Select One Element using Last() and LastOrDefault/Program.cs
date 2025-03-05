261c2e9b5dded3c07d05d5292abb1f953c4d4d18
MohamadKhaled <mohamadknsult2@gmail.com>
2025-03-05 15:12:59 +0200

    Select One Element using Last() and LastOrDefault

---
+++ Language_Integrated_Query/Select One Element using Last() and LastOrDefault/Program.cs
@@ -0,0 +1,30 @@
+﻿using SelectOneElement;
+
+var cars = CarRepository.GetCars();
+
+// var resultQ = (from car in cars
+//                where IsMaserati(car)
+// where car.Manufacturer == "Maserati"
+//                select car).LastOrDefault();
+// CarRepository.PrintCars([resultQ]);
+
+// var allCars = cars.ToList();
+// CarRepository.PrintCars(allCars);
+cars = [];
+// var resultM = cars
+// .Where(IsMaserati())
+//     .Last(IsMaserati);
+// CarRepository.PrintCars([resultM]);
+
+var resultQOR = cars.LastOrDefault(IsMaserati);
+// CarRepository.PrintCars([resultQOR]);
+
+if (resultQOR != null)
+    CarRepository.PrintCars(new List<Car> { resultQOR });
+else
+    Console.WriteLine("No car found with the specified manufacturer.");
+
+static bool IsMaserati(Car car)
+{
+    return car.Manufacturer == "Maseratii";
+}
