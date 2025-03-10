f8da7200ecc865d3640e3e1fe649fb31aea3dae8
MohamadKhaled <mohamadknsult2@gmail.com>
2025-03-10 07:14:48 +0200

    Add new element using Append() and Prepend() in LINQ

---
+++ Language_Integrated_Query/Add New Element using Append() and Prepend()/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Append_Prepend;
+using System.Collections.Generic;
+using System.Linq;
+
+var cars = CarRepository.GetCars();
+
+var newCar = new Car(28, "Chevrolet", "Camaro", "2023", "Convertible", "Gasoline", "Red", "ZD67KZP", 190);
+
+var result = cars
+        .Append(newCar)
+        .Prepend(newCar);
+// var result = cars.Prepend(newCar);
+CarRepository.PrintCars(result);
