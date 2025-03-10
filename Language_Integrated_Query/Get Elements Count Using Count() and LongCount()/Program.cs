013d94e81c2ae8d90cb26917fb28814fea5fec36
MohamadKhaled <mohamadknsult2@gmail.com>
2025-03-10 07:30:36 +0200

    Get elements count using Count() and LongCount()

---
+++ Language_Integrated_Query/Get Elements Count Using Count() and LongCount()/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GetElementsCountUsingCountAndLongCount;
+using System.Collections.Generic;
+using System.Linq;
+
+var cars = CarRepository.GetCars();
+
+var carsCount = cars.Count();
+var fordCount = cars.Count(IsFord);
+var fordLongCount = cars.LongCount(IsFord);
+System.Console.WriteLine($"Total Ford Cars: {fordLongCount}");
+
+static bool IsFord(Car car) => car.Manufacturer == "Ford";
