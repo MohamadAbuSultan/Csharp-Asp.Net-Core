a73f7e29889a13ed87a994a28b6f0b6f12a390a0
MohamadKhaled <mohamadknsult2@gmail.com>
2025-03-10 07:41:21 +0200

    Get elements count using CountBy() in LINQ

---
+++ Language_Integrated_Query/Get Elements Count Using CountBy()/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GetElementsCountUsingCountBy;
+using System.Collections.Generic;
+using System.Linq;
+
+var cars = CarRepository.GetCars();
+
+// var makesCount = cars.CountBy(c => c.Manufacturer).OrderByDescending(c => c.Value);
+var makesCount = cars.CountBy(c => $"{c.Manufacturer}~{c.Color}").OrderByDescending(c => c.Value);
+foreach (var make in makesCount)
+{
+    Console.WriteLine($"{make.Key} --> {make.Value}");
+}
+
