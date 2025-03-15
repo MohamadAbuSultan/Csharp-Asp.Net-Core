21ddaae262f9b3887ef55f996bcc7487f96f4970
MohamadKhaled <mohamadknsult2@gmail.com>
2025-03-15 06:25:51 +0200

    .

---
+++ ASP.NET Core/JokesWebApp/JokesWebApp/Models/Joke.cs
@@ -0,0 +1,11 @@
+public class Joke
+{
+    public int Id { get; set; }
+    public string JokeQuestion { get; set; }
+    public string JokeAnswer { get; set; }
+
+    public Joke(Parameters)
+    {
+        
+    }
+}
