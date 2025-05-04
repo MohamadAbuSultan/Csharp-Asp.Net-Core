c24ff52cddcf4b8cd6571b3b6620c003f89adbc1
MohamadKhaled <mohamadknsult2@gmail.com>
2025-05-04 08:33:00 +0300

    Implement Sudoku solver with backtracking algorithm

---
+++ Problem-Solving Techniques/Backtracking/Program.cs
@@ -0,0 +1,74 @@
+﻿const int SIZE = 9;
+
+int[,] board =
+{
+    { 5, 3, 0, 0, 7, 0, 0, 0, 0 },
+    { 6, 0, 0, 1, 9, 5, 0, 0, 0 },
+    { 0, 9, 8, 0, 0, 0, 0, 6, 0 },
+    { 8, 0, 0, 0, 6, 0, 0, 0, 3 },
+    { 4, 0, 0, 8, 0, 3, 0, 0, 1 },
+    { 7, 0, 0, 0, 2, 0, 0, 0, 6 },
+    { 0, 6, 0, 0, 0, 0, 2, 8, 0 },
+    { 0, 0, 0, 4, 1, 9, 0, 0 ,5},
+    {0, 0, 0, 0, 8, 0, 0, 7, 9}
+};
+
+SolveSudoku(board);
+Console.WriteLine("Sudoku Board:");
+PrintBoard(board);
+
+void PrintBoard(int[,] board)
+{
+    for (int row = 0; row < SIZE; row++)
+    {
+        for (int col = 0; col < SIZE; col++)
+        {
+            Console.Write(board[row, col] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+bool SolveSudoku(int[,] board)
+{
+    for (int row = 0; row < SIZE; row++)
+    {
+        for (int col = 0; col < SIZE; col++)
+        {
+            if (board[row, col] == 0)
+            {
+                for (int num = 1; num <= SIZE; num++)
+                {
+                    if (IsValid(board, row, col, num))
+                    {
+                        board[row, col] = num;
+
+                        if (SolveSudoku(board))
+                            return true;
+
+                        board[row, col] = 0;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool IsValid(int[,] board, int row, int col, int num)
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        int boxRowStart = row - (row % 3);
+        int boxColStart = col - (col % 3);
+
+        if (board[row, i] == num
+        || board[i, col] == num
+        || board[boxRowStart + i / 3, boxColStart + i % 3] == num)
+        {
+            return false;
+        }
+    }
+    return true;
+}
---
+++ Problem-Solving Techniques/Backtracking/README.md
@@ -0,0 +1,14 @@
+# Backtracking Problem-Solving Technique
+
+Backtracking is an algorithmic method that incrementally tests possibilities and undoes them when they lead nowhere.
+
+## When to Use
+- Explore multiple possible solutions to the problem.
+- A solution can be built incrementally, one decision at a time.
+- Each choice affects subsequent choices, requiring a step-by-step approach.
+
+## Applications of Backtracking Algorithm
+- **Solving puzzles** (e.g., Sudoku).
+- **Determining the shortest maze path**.
+- **Addressing scheduling tasks**.
+- **Allocating resources** efficiently.
