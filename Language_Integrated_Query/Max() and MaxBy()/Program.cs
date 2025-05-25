d028efc1b1c36b85ff876389f5ec9a2689124c3e
MohamadKhaled <mohamadknsult2@gmail.com>
2025-05-25 14:08:52 +0300

    Max in C# LINQ

---
+++ Language_Integrated_Query/Max() and MaxBy()/Program.cs
@@ -0,0 +1,13 @@
+﻿using MaxAndMaxBy;
+
+int[] numbers = { 1, 2, 3, 4, 5 };
+
+int maxNumber = numbers.Max();
+// Console.WriteLine($"The maximum number is: {maxNumber}");
+
+var cars = CarRepository.GetCars();
+int maxSpeed = cars.Max(car => car.MaxSpeed);
+// Console.WriteLine($"The maximum speed is: {maxSpeed}");
+
+int maxLength = cars.Max(car => car.Manufacturer.Length);
+Console.WriteLine($"The maximum length of a manufacturer name is: {maxLength}");
