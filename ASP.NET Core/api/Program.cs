6d9ccf2cdcaa50486528427d16aaa8e6015226df
MohamadKhaled <mohamadknsult2@gmail.com>
2025-05-31 00:22:03 +0300

    Entity Framework

--- ASP.NET Core/api/Program.cs
+++ ASP.NET Core/api/Program.cs
@@ -1,8 +1,16 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
--- ASP.NET Core/api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ ASP.NET Core/api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a283164109fa2f1f9b4151f4a6086b1c771d9461")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1132dfb41932fdb993e6583a6bf62bbd78f2c401")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
