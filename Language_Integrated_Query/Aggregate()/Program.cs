af43b533e390ee3023bd76fe808d75ca749041e2
MohamadKhaled <mohamadknsult2@gmail.com>
2025-06-24 04:16:13 +0300

    Aggregate in C# LINQ

---
+++ Language_Integrated_Query/Aggregate()/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] numbers = { 1, 2, 3, 4, 5 };
+
+int result = numbers.Aggregate((acc, next) =>
+{
+    // Console.WriteLine($"acc: {acc}, next: {next}");
+    return acc + next;
+});
+
+string sentence = "The quick brown fox jumps over the lazy dog";
+
+string[] words = sentence.Split(' ');
+
+string concatenated = words.Aggregate((acc, next) =>
+{
+    // Console.WriteLine($"acc: {acc}, next: {next}");
+    return acc + " " + next;
+});
+
+System.Console.WriteLine($"Result of aggregation: {concatenated}");
