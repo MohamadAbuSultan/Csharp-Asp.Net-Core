3855034b629a88df21f397796de742260d492cf1
MohamadKhaled <mohamadknsult2@gmail.com>
2025-10-29 09:30:27 +0300

    WelcomeBack

--- ASP.NET Core/WelcomeBack/WelcomeBack/Program.cs
+++ ASP.NET Core/WelcomeBack/WelcomeBack/Program.cs
@@ -1,4 +1,3 @@
-
 namespace WelcomeBack
 {
     public class Program
@@ -6,27 +5,9 @@ namespace WelcomeBack
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
-
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
+            app.MapGet("/", () => "Hello World!");
 
             app.Run();
         }
