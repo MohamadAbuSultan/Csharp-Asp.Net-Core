3552d6b4cfabf9f47f63b5a7e7edff1980c1fb23
MohamadKhaled <mohamadknsult2@gmail.com>
2025-11-02 14:21:17 +0200

    Middleware

--- ASP.NET Core/WelcomeBack/WelcomeBack/Program.cs
+++ ASP.NET Core/WelcomeBack/WelcomeBack/Program.cs
@@ -5,9 +5,23 @@ namespace WelcomeBack
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
             var app = builder.Build();
 
             app.MapGet("/", () => "Hello World!");
+            app.MapGet("/hello/{name:alpha}", (string name) => $"Hello {name}!");
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Hello from 1nd delegate. ");
+                await next.Invoke();
+                await context.Response.WriteAsync(" Hello from 1nd delegate. after");
+            });
+
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync(" Hello from 2nd delegate.");
+            });
 
             app.Run();
         }
--- ASP.NET Core/WelcomeBack/WelcomeBack/obj/Debug/net9.0/WelcomeBack.AssemblyInfo.cs
+++ ASP.NET Core/WelcomeBack/WelcomeBack/obj/Debug/net9.0/WelcomeBack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WelcomeBack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3666c7018de61e7b3b6b591b6bf065452d6394d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3855034b629a88df21f397796de742260d492cf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WelcomeBack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WelcomeBack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
