882c66c3337bdbb3b522c693cfe886fc4afe26c2
MohamadKhaled <mohamadknsult2@gmail.com>
2025-12-08 18:29:55 +0200

    Flexible Data Container

---
+++ C# Projects/Flexible Data Container/Flexible Data Container/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Flexible_Data_Container
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataContainer<int> intContainer = new DataContainer<int>(42);
+            intContainer.PrintData();
+
+            DataContainer<string> stringContainer = new DataContainer<string>("Hello, World!");
+            stringContainer.PrintData();
+        }
+    }
+
+    internal class DataContainer<T>
+    {
+        private T _data;
+
+        public DataContainer(T data)
+        {
+            _data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine($"Stored Data: {_data}, Type: {_data.GetType()}");
+        }
+    }
+}
