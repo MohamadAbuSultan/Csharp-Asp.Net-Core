63437200791309d811ac26ce0b511ba191cd02c6
MohamadKhaled <mohamadknsult2@gmail.com>
2025-12-08 19:59:29 +0200

    Prime Number Validator

---
+++ C# Projects/Prime Number Validator/Prime Number Validator/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Prime_Number_Validator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isPrime = PrimeNumberValidator.IsPrime(7);
+            Console.WriteLine(isPrime);
+        }
+    }
+
+    internal static class PrimeNumberValidator
+    {
+        public static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+                if (number % i == 0) return false;
+            
+            return true;
+        }
+    }
+}
