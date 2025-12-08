349b43605b13344b51af4ec04cba41afffe4b7a7
MohamadKhaled <mohamadknsult2@gmail.com>
2025-12-08 15:05:05 +0200

    Generic Data Swapper

---
+++ C# Projects/Generic Data Swapper/Generic Data Swapper/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Generic_Data_Swapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10, y = 20;
+            // No need to 'new' the class if the method is static
+            GenericMethods.SwapData(ref x, ref y);
+            Console.WriteLine($"After swapping integers: x = {x}, y = {y}");
+
+            string str1 = "Hello", str2 = "World";
+            GenericMethods.SwapData(ref str1, ref str2);
+            Console.WriteLine($"After swapping strings: str1 = {str1}, str2 = {str2}");
+        }
+    }
+    internal static class GenericMethods
+    {
+        // Making this 'static' makes it a utility method accessible anywhere
+        public static void SwapData<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
