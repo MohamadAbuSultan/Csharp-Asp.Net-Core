a2df17383ae3f5a91006234cdbffe79bc34947f3
MohamadKhaled <mohamadknsult2@gmail.com>
2025-12-09 16:12:01 +0200

    FizzBuzz (The Logical Switch)

---
+++ C# Projects/FizzBuzz (The Logical Switch)/FizzBuzz (The Logical Switch)/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace FizzBuzz__The_Logical_Switch_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LogicalSwitch.PrintNumbersFrom1to30();
+        }
+    }
+
+    internal static class LogicalSwitch
+    {
+        public static void PrintNumbersFrom1to30()
+        {
+            for (int i = 1; i <= 30; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0) Console.WriteLine("FizzBuzz");
+                else if (i % 3 == 0) Console.WriteLine("Fizz");
+                else if (i % 5 == 0) Console.WriteLine("Buzz");
+                else Console.WriteLine(i);
+            }
+
+        }
+    }
+}
