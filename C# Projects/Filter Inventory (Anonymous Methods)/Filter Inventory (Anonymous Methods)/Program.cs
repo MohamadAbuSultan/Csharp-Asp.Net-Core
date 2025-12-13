7b8987032d39228a45ca0ac5d993a1c242ebc570
MohamadKhaled <mohamadknsult2@gmail.com>
2025-12-13 08:36:24 +0200

    Filter Inventory using Anonymous Methods in C#

---
+++ C# Projects/Filter Inventory (Anonymous Methods)/Filter Inventory (Anonymous Methods)/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Filter_Inventory__Anonymous_Methods_
+{
+    public delegate bool ProductFilter(Product filter);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> product = new List<Product>()
+            {
+        new Product { Name = "Laptop", Price = 1200.00m },
+        new Product { Name = "Mouse", Price = 25.50m },
+        new Product { Name = "Keyboard", Price = 80.00m },
+        new Product { Name = "Monitor", Price = 300.00m }
+            };
+
+
+            ProductFilter filter = delegate (Product p)
+            {
+                return p.Price > 50.00m;
+            };
+
+            //FilterProducts(products, p => p.Price > 50.00m); // lambda 
+            FilterProducts(product, filter);
+        }
+
+        private static void FilterProducts(List<Product> product, ProductFilter filter)
+        {
+            foreach (var item in product)
+            {
+                if (filter(item))
+                    Console.WriteLine($"Product: {item.Name}, Price: ${item.Price}");
+            }
+        }
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
