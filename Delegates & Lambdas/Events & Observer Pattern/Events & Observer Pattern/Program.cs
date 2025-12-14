b2f276f0932c26d97c6663bf74be18abc2b6ace7
MohamadKhaled <mohamadknsult2@gmail.com>
2025-12-14 08:49:15 +0200

    Intro To Events

---
+++ Delegates & Lambdas/Events & Observer Pattern/Events & Observer Pattern/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Events___Observer_Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Observer Pattern
+
+            YoutubeChannel channel = new YoutubeChannel();
+
+            Subscriber sub1 = new Subscriber();
+            Subscriber sub2 = new Subscriber();
+            Subscriber sub3 = new Subscriber();
+
+            sub1.Subscribe(channel);
+            sub2.Subscribe(channel);
+            sub3.Subscribe(channel);
+
+            //channel.UploadVideo("video1");
+            //channel.UploadVideo("video2");
+            //channel.UploadVideo("video3");
+
+            //channel.videoUpload("Delegate"); // problem- direct invoke
+            //channel.videoUpload = null; // problem#2- setting delegate to null    
+
+            channel.videoUpload += (string title) => Console.WriteLine(title);
+
+        }
+    }
+
+    public delegate void VideoUpload(string title);
+
+    public class YoutubeChannel
+    {
+        public event VideoUpload videoUpload;
+        public void UploadVideo(string videoTitle)
+        {
+            Console.WriteLine($"New video uploaded {videoTitle}");
+            videoUpload.Invoke(videoTitle);
+        }
+    }
+
+    public class Subscriber
+    {
+        public void Subscribe(YoutubeChannel ytChannel)
+        {
+            ytChannel.videoUpload += WatchTheVideo;
+        }
+
+        public void WatchTheVideo(string videoTitle) // handler
+        {
+            Console.WriteLine($"User watch {videoTitle}");
+        }
+    }
+}
